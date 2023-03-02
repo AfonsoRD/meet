6ed857934cb878ef080308776d55b820619c47d2
Rui Afonso <afonso.r.dev@gmail.com>
2023-03-02 15:51:16 +0100

    Render summary and description of the event

--- src/Event.js
+++ src/Event.js
@@ -1,8 +1,22 @@
 import React, { Component } from 'react';
 
 class Event extends Component {
+  state = { collapsed: true };
+  toggleDetails = () => {
+    this.setState((prevState) => ({
+      collapsed: !prevState.collapsed
+    }));
+  };
   render() {
-    return <div></div>;
+    const { event } = this.props;
+
+    return (
+      <div>
+        <h1 className='summary'>{event.summary}</h1>
+
+        <p className='description'> {event.description}</p>
+      </div>
+    );
   }
 }
 

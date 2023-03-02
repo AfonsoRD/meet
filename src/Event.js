170f8650580849cabbff442285cdbc965c54da7c
Rui Afonso <afonso.r.dev@gmail.com>
2023-03-02 16:07:16 +0100

    Create objects to be shown either expanded or collapsed view

--- src/Event.js
+++ src/Event.js
@@ -9,12 +9,37 @@ class Event extends Component {
   };
   render() {
     const { event } = this.props;
+    const { collapsed } = this.state;
 
     return (
       <div>
         <h1 className='summary'>{event.summary}</h1>
+        <p className='event-start'>
+          {new Date(event.start.dateTime).toString()}
+        </p>
+        <p className='event-location'>
+          {`@${event.summary} | ${event.location}`}
+        </p>
 
-        <p className='description'> {event.description}</p>
+        {!collapsed && (
+          <>
+            <h2 className='about'>About event:</h2>
+            <a
+              className='link'
+              href={event.htmlLink}
+            >
+              See details on Google Calendar
+            </a>
+            <p className='description'>{event.description}</p>
+          </>
+        )}
+
+        <button
+          className='details-button'
+          onClick={() => this.toggleDetails()}
+        >
+          {collapsed ? 'show' : 'hide'} details
+        </button>
       </div>
     );
   }

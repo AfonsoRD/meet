4eb6bcaeb48ddfad046c9f327cd4c6fc2d395005
Rui Afonso <afonso.r.dev@gmail.com>
2023-03-02 16:17:56 +0100

    render NumberOfEvents component

---
+++ src/NumberOfEvents.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+
+class NumberOfEvents extends Component {
+  state = { num: 32 };
+
+  changeNum(value) {
+    this.setState({ num: value });
+  }
+
+  render() {
+    const { num } = this.state;
+
+    return (
+      <div className='NumverOfEvents'>
+        <input
+          className='num'
+          type='number'
+          value={this.num}
+          onChange={(event) => {
+            this.changeNum(event.target.value);
+          }}
+        />
+      </div>
+    );
+  }
+}
+
+export default NumberOfEvents;

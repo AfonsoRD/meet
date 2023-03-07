94fe04244e724a9f5cee4258c65bcee76cf87a39
Rui Afonso <afonso.r.dev@gmail.com>
2023-03-07 20:52:05 +0100

    Change the way how to render DOM

--- src/index.js
+++ src/index.js
@@ -1,9 +1,29 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+// import ReactDOM from 'react-dom/client'; <- This import is only for React version 18
+import { render } from 'react-dom'; // <- This is the correct import statement for React version 17
 import './index.css';
 import App from './App';
-// import * as serviceWorkerRegistration from './serviceWorkerRegistration';
-import { register, unregister } from './serviceWorkerRegistration';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
+import reportWebVitals from './reportWebVitals';
+
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = document.getElementById('root'); // <- This is the correct method call for React version 17
+render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  root
+);
+
+serviceWorkerRegistration.register();
+reportWebVitals();
+
+/*
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -16,9 +36,10 @@ root.render(
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://cra.link/PWA
-register();
+serviceWorkerRegistration.register();
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+*/

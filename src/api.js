21fd126b847a0f2f9b493cb43ad0ab671bb550e0
Rui Afonso <afonso.r.dev@gmail.com>
2023-03-08 17:55:55 +0100

    Create getToken and removeQuery functions

--- src/api.js
+++ src/api.js
@@ -65,3 +65,32 @@ export const getAccessToken = async () => {
   }
   return accessToken;
 };
+
+const removeQuery = () => {
+  if (window.history.pushState && window.location.pathname) {
+    var newurl =
+      window.location.protocol +
+      '//' +
+      window.location.host +
+      window.location.pathname;
+    window.history.pushState('', '', newurl);
+  } else {
+    newurl = window.location.protocol + '//' + window.location.host;
+    window.history.pushState('', '', newurl);
+  }
+};
+
+const getToken = async (code) => {
+  const encodeCode = encodeURIComponent(code);
+  const { access_token } = await fetch(
+    'YOUR_GET_ACCESS_TOKEN_ENDPOINT' + '/' + encodeCode
+  )
+    .then((res) => {
+      return res.json();
+    })
+    .catch((error) => error);
+
+  access_token && localStorage.setItem('access_token', access_token);
+
+  return access_token;
+};

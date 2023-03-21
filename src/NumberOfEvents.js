f3515e18717826bd5209f63824dae6336a72dee5
Rui Afonso <afonso.r.dev@gmail.com>
2023-03-21 16:44:37 +0100

    Add Error alert

--- src/NumberOfEvents.js
+++ src/NumberOfEvents.js
@@ -1,21 +1,31 @@
 import React, { Component } from 'react';
+import { ErrorAlert } from './Alert';
 
 class NumberOfEvents extends Component {
   state = { num: 32 };
 
   handleInputChanged = (event, props) => {
     let inputValue = event.target.value;
-    if (inputValue < 0) inputValue = 0;
+    let errorText;
+
+    // don't even allow negative numbers; come on now.
+    if (inputValue < 0 || inputValue === 0 || inputValue === '0')
+      inputValue = 0;
+    if (!inputValue) errorText = 'Select at least 1 event';
+    else if (inputValue >= 50) {
+      errorText = '50 max. events per page';
+      inputValue = 50;
+    }
+
     this.props.updateEvents(null, inputValue);
-    this.setState({
-      num: inputValue
-    });
+    this.setState({ num: inputValue, errorText: errorText });
   };
 
   render() {
     const { num } = this.state;
     return (
       <div className='NumverOfEvents'>
+        <ErrorAlert text={this.state.errorText} />
         <h3 className='event-num'>Events list:</h3>
         <input
           className='num-input'
